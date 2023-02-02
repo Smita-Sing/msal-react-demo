03a30a31f431e88594a3a9548d3921424144c393
smita singh <smita7jan.singh@gmail.com>
2023-02-02 21:04:31 +0530

    poc

--- src/App.js
+++ src/App.js
@@ -5,17 +5,46 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Profile } from "./pages/Profile";
 
-function App() {
+import { MsalProvider, useMsal, useIsAuthenticated } from '@azure/msal-react';
+import { useEffect} from 'react';
+import {InteractionRequiredAuthError} from '@azure/msal-browser';
+
+function App({msalInstance}) {
     return (
+        <MsalProvider instance= {msalInstance}>
         <PageLayout>
             <Grid container justifyContent="center">
                 <Pages />
             </Grid>
         </PageLayout>
+        </MsalProvider>
     );
 }
 
 const Pages = () => {
+    const {instance}= useMsal();
+    const isAuthenticated= useIsAuthenticated();
+    
+    useEffect(()=>{
+        if (!isAuthenticated){
+            /*grab username from query params*/
+
+            instance.ssoSilent({
+                scopes:['user.read'],
+                loginHint:'ssingh@corevitas.com'
+        }).then((response)=>{
+            instance.setActiveAccount(response.account);
+
+        }).catch((error)=>{
+            if (error instanceof InteractionRequiredAuthError){
+                instance.loginRedirect({
+                    scopes:['user.read'],
+                })
+            }
+        });        
+    }
+    },[])
+    
     return (
         <Routes>
             <Route path="/" element={<Home />} />
--- src/index.js
+++ src/index.js
@@ -8,12 +8,42 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from './App';
 
+import { PublicClientApplication, EventType } from '@azure/msal-browser';
+
+const pca= new PublicClientApplication ({
+    auth: {
+        clientId:'7ca85254-007b-4341-b179-7802597c4f23',
+        authority: 'https://login.microsoftonline.com/51ecbc2a-b275-4f06-96ef-b7512fe0fad0',
+        redirectUri: '/'
+    },
+    cache:{
+        cacheLocation: 'localStorage',
+        storeAuthStateInCookie: false,
+
+    },
+    system: {
+        loggerOptions:{
+            loggerCallback:(level, message, containsPii)=>{
+                console.log(message)
+            },
+            logLevel:'info',
+        } 
+    }
+});
+
+pca.addEventCallback(event=>{
+    if (event.eventType=== EventType.LOGIN_SUCCESS){
+        console.log(event);
+        pca.setActiveAccount(event.payload.account);
+    }
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
             <ThemeProvider theme={theme}>
-                <App />
+                <App msalInstance= {pca} />
             </ThemeProvider>
         </BrowserRouter>
     </React.StrictMode>
